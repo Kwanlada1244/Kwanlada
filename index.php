2784b48a01e661aa655a160796d83a7536fbd62e
Kwanlada1244 <73695120+Kwanlada1244@users.noreply.github.com>
2020-10-30 17:53:36 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Hello world</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>My First Heading</h1>
+        <p>My First Paragraph</p>
+        <p>Name: Kwanlada Boonyachotima</p>
+        <p>Nickname: Pim</p>
+        <img src="6997052-funny-cat.jpg">
+        <img src="WBCL_web_2014_blog_longhair_shorthair-858x429.jpg">
+        <p>เรืองตลก: มีเด็กขี้สงสัยอยู่คนนึงถามแม่ว่า "ทำไมผมแม่ถึงเริ่มมีผมสีเทาขึ้นแล้วล่ะ"</p>
+        <p>แม่อยากจะใช้โอกาสนี้เพื่อสอนลูก จึงตอบกลับไปว่า"ก็เป็นเพราะลูกยังไงล่ะ เวลาที่ลูกทำตัวไม่ดี1ครั้ง ผมแม่ก็จะกลายเป็นสีเทา1เส้น"</p>
+        <p>เด็กคนนั้นก็เลยตอบกลับอย่างไร้เดียงสาว่า "อ๋ออ ตอนนี้ผมเข้าใจแล้วว่า ทำไมผมของคุณยายถึงมีแต่สีเทา"</p>
+        <br>
+        <a herf="http://www.it.kmitl.ac.th">This is a Link</a>
+
+        <h2>The unordered list</h2>
+        <u1>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </u1>
+
+    </body>
+</html>
