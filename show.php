f57b104588ef1dd46e6e46fedc0b27e0bb4ff833
Kwanlada1244 <73695120+Kwanlada1244@users.noreply.github.com>
2020-11-20 20:26:02 +0700

    Update show.php

--- show.php
+++ show.php
@@ -31,7 +31,7 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
-    <td><a href="form_delete.html"><input type="submit" value="Delete"  class="btn btn-danger"></td>
+    <td><a href="delete_form.html"><input type="submit" value="Delete"  class="btn btn-danger"></td>
   </tr>
 <?php
 }
