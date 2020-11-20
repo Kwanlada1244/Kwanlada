9ac60c4381365eef02214f47c8c504071c412c32
Kwanlada1244 <73695120+Kwanlada1244@users.noreply.github.com>
2020-11-20 13:47:15 +0700

    Add files via upload

---
+++ insert_new.php
@@ -0,0 +1,25 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'kwanlada.mysql.database.azure.com', 'kwanlada@kwanlada', 'CTQply15', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_GET['name'];
+$comment = $_GET['comment'];
+
+
+$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
