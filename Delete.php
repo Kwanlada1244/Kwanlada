6cccd95ae927e0fc3f99dcf557b7704a4f2566f4
Kwanlada1244 <73695120+Kwanlada1244@users.noreply.github.com>
2020-11-20 13:09:10 +0700

    Add files via upload

---
+++ Delete.php
@@ -0,0 +1,19 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'kwanlada.mysql.database.azure.com', 'kwanlada@kwanlada', 'CTQply15', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$id = $_GET['id'];
+$sql = "DELETE FROM guestbook WHERE id = $id";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Record deleted successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
