75d56445252bcd3f5a919734119f8ef2d3d53b66
Kwanlada1244 <73695120+Kwanlada1244@users.noreply.github.com>
2020-11-20 13:22:02 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,5 +1,4 @@
 <?php
-
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'kwanlada.mysql.database.azure.com', 'kwanlada@kwanlada', 'CTQply15', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
@@ -7,7 +6,7 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 $id = $_GET['id'];
-$sql = "DELETE FROM guestbook WHERE ID = $id";
+$sql = "DELETE FROM guestbook WHERE id = $id";
 
 if (mysqli_query($conn, $sql)) {
     echo "Record deleted successfully";
